f197c74d903548f7bc4475b4d3ab4a4d79ec9f2c
Yutenko <meisteremka@googlemail.com>
2021-09-28 22:01:11 +0200

    Add files via upload

---
+++ precache-manifest.b4520202ccf022f9b1b60a0be6cfb424.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68bd789cf27058142bdd4cf0f124e74c",
+    "url": "/appszauberwilli/spicker/build/index.html"
+  },
+  {
+    "revision": "9921f52cf0df7a87a546",
+    "url": "/appszauberwilli/spicker/build/static/css/main.a3800748.chunk.css"
+  },
+  {
+    "revision": "dd55f5d53151f8e0f97d",
+    "url": "/appszauberwilli/spicker/build/static/js/2.9712c431.chunk.js"
+  },
+  {
+    "revision": "9921f52cf0df7a87a546",
+    "url": "/appszauberwilli/spicker/build/static/js/main.feb77de8.chunk.js"
+  },
+  {
+    "revision": "8fe99763714810cbf99e",
+    "url": "/appszauberwilli/spicker/build/static/js/runtime~main.a0b5483d.js"
+  }
+]);
